263e4efe0d8be5b5efd5d394399a9f98e0dae5f4
Elias Rodrigues <49626719+eliasantoniorodrigues1@users.noreply.github.com>
2023-05-17 18:32:49 +0000

    Update and rename main.ipynb to main.py

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import pandas as pd
+from google.cloud import bigquery
+
+# this code needs to be at google colab to work properly
 from google.colab import auth
 auth.authenticate_user()
 
-project_id = 'ga360-localiza'
-table = 'crm_analytics.moedas_historico_cotacoes'
+# project data
+project_id = 'your_project_id'
+table = 'dataset.moedas_historico_cotacoes'
 
+# client data
+client = bigquery.Client(project=project_id)
+table = client.get_table(table)
+
+# dataset
 df = pd.read_csv('currency_history_rates_USD_missing.csv')
-df
 
 new_columns = ['paridade', 'nome', 'unidades_por_codigo', 'code_por_unidade', 'data_atualizacao', 'codigo']
 df.drop(columns=['index'], inplace=True)
 df.columns = new_columns
 
-df
-
 columns = df.columns.tolist()
 print(columns)
 
+# changing position of columns on dataset
 new_position_column = df.pop(columns[-1])
 df.insert(0, 'codigo', new_position_column)
-
-df
-
-from google.cloud import bigquery
-
-client = bigquery.Client(project=project_id)
-table = client.get_table(table)
-table
 
 # dict compherension
 generate_schema = [{'name': i.name, 'type': i.field_type} for i in table.schema]
